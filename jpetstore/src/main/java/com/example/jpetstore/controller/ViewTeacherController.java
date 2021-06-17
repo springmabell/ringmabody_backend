b9db772c5cc39b4a1213d82c0b67c0b6d6b5c225
suejin98 <75103521+suejin98@users.noreply.github.com>
2021-06-17 23:20:25 +0900

    Add files via upload

---
+++ jpetstore/src/main/java/com/example/jpetstore/controller/ViewTeacherController.java
@@ -0,0 +1,42 @@
+package com.example.jpetstore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.jpetstore.domain.Item;
+import com.example.jpetstore.domain.Order;
+import com.example.jpetstore.domain.TeacherAccount;
+import com.example.jpetstore.service.PetStoreFacade;
+
+/**
+ * @author Juergen Hoeller
+ * @since 30.11.2003
+ * @modified-by Changsup Park
+ */
+@Controller
+public class ViewTeacherController { 
+
+	private PetStoreFacade petStore;
+
+	@Autowired
+	public void setPetStore(PetStoreFacade petStore) {
+		this.petStore = petStore;
+	}
+
+	@RequestMapping("/teacher/viewTeacher.do")
+	public String handleRequest(
+			@RequestParam("teacher_id") String teacher_id,
+			ModelMap model) throws Exception {
+		TeacherAccount teacherAccount = this.petStore.getTeacherAccount(teacher_id);
+		model.put("teacherAccount", teacherAccount);
+		return "TeacherAccount";
+
+	
+		}
+}
