a038f1efaefde20dac203044f16bdce87f59d1cc
suejin98 <75103521+suejin98@users.noreply.github.com>
2021-06-20 00:41:19 +0900

    Add files via upload

---
+++ jpetstore/src/main/java/com/example/jpetstore/dao/TeacherAccountDao.java
@@ -0,0 +1,22 @@
+package com.example.jpetstore.dao;
+
+import java.util.List;
+import org.springframework.dao.DataAccessException;
+import com.example.jpetstore.domain.Account;
+import com.example.jpetstore.domain.TeacherAccount;
+
+public interface TeacherAccountDao {
+	
+  List<TeacherAccount> getAllTeacherAccount() throws DataAccessException;
+  
+  List<TeacherAccount> getAllClass() throws DataAccessException;
+
+  TeacherAccount getTeacherAccount(String teacher_id);
+
+  TeacherAccount getTeacherAccount(String teacher_id, String teacher_pwd);
+
+  void insertTeacherAccount(TeacherAccount account);
+
+  void updateTeacherAccount(TeacherAccount account);
+
+}
