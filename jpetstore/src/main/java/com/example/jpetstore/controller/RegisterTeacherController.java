7de965a23726025ffa5f69f01c811f9df6d936f9
naeun song <aoghk23@gmail.com>
2021-06-20 22:18:33 +0900

    회원가입

--- jpetstore/src/main/java/com/example/jpetstore/controller/RegisterTeacherController.java
+++ jpetstore/src/main/java/com/example/jpetstore/controller/RegisterTeacherController.java
@@ -8,13 +8,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.WebUtils;
 import com.example.jpetstore.domain.Category;
+import com.example.jpetstore.domain.Class;
 import com.example.jpetstore.domain.Product;
+import com.example.jpetstore.service.MainFacade;
 import com.example.jpetstore.service.PetStoreFacade;
 
 /**
@@ -28,6 +31,12 @@ public class RegisterTeacherController {
 
 	@Value("thyme/join_teacher_form")
 	private String formViewName;
+	
+	
+
+	@Autowired
+	private MainFacade mainFacade;
+	
 	
 	@Autowired
 	private PetStoreFacade petStore;
@@ -55,7 +64,10 @@ public class RegisterTeacherController {
 	public String onSubmit(
 			HttpServletRequest request, HttpSession session,
 			@ModelAttribute("teacherAccountForm") TeacherAccountForm teacherAccountForm,
+			Model model,
 			BindingResult result) throws Exception {
+		
+		System.out.println("pass 1");
 
 		if (result.hasErrors()) return formViewName;
 		try {
@@ -76,6 +88,14 @@ public class RegisterTeacherController {
 			petStore.getTeacherAccount(teacherAccountForm.getAccount().getTeacher_id()));
 		
 		session.setAttribute("teacherSession", teacherSession);
+		
+
+		List<Class> endingSoonList = mainFacade.endingSoon();
+		List<Class> bestClassList = mainFacade.bestClass();
+		model.addAttribute("endingSoonList", endingSoonList);
+		model.addAttribute("bestClassList", bestClassList);
+		model.addAttribute("name", teacherSession.getAccount().getTeacher_name());
+
 		
 		return "thyme/main";  
 	}
